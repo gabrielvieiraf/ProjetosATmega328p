59c939d322ca0d5bfaac91ca95212c180da3a70b
Gabriel Vieira <48156370+gabrielvieiraf@users.noreply.github.com>
2020-07-02 00:45:30 -0300

    Comentários adicionados

--- Projetos/Sensor DS18B20/Testador.ino
+++ Projetos/Sensor DS18B20/Testador.ino
@@ -9,21 +9,13 @@ const int numCols = 16;
 
 OneWire  ds(2);  // Pino do Sinal do sensor
 
-/*
-    lcd.setCursor(0, 0); // top left
-    lcd.setCursor(15, 0); // top right
-    lcd.setCursor(0, 1); // bottom left
-    lcd.setCursor(15, 1); // bottom right
-*/
-
-
 void setup(void) {
   Serial.begin(9600);
   lcd.begin(numCols, numRows);
 }
 
 void loop(void) {
-    
+
   byte i;
   byte present = 0;
   byte type_s;
@@ -31,90 +23,73 @@ void loop(void) {
   byte addr[8];
   float celsius;
 
-
   ds.reset();
   ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  ds.write(0x44, 1);        // Começa conversão com o sensor em modo parasita
 
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
+  delay(1000);
 
   present = ds.reset();
   ds.select(addr);
-  ds.write(0xBE);         // Read Scratchpad
+  ds.write(0xBE);         // Lê Scratchpad
 
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+  for ( i = 0; i < 9; i++) {           // Precisamos de 9 bytes
     data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
   }
-  
+
   int16_t raw = (data[1] << 8) | data[0];
   if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
+    raw = raw << 3; // 9 bit como padrão
     if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
+      // "count remain" lhe dá resolução de 12 bit
       raw = (raw & 0xFFF0) + 12 - data[6];
     }
   } else {
     byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit res, 93.75 ms
     else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
     else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
+    //// Por padrão, utilizamos a resolução de 12 bit, tempo de conversão de 750 ms
   }
-  celsius = (float)raw / 16.0;
+  
+  celsius = (float)raw / 16.0; //Conversão da leitura para graus celsius
 
-    lcd.clear();
+  lcd.clear();
 
-    
   if ( !ds.search(addr)) {
     ds.reset_search();
-    lcd.setCursor(9,0);
-    if(celsius <= 0 || celsius >= 50) {
-      for(int positionCounter = 0; positionCounter <40; positionCounter ++) {
-      lcd.setCursor(1,0);
-      lcd.scrollDisplayLeft();  //Essa é a função que faz as letras se deslocarem
-      lcd.print("Insira a Sonda ou verifique a Crimpagem");
-      delay(250);
-        }
+    lcd.setCursor(9, 0);
+    if (celsius <= 0 || celsius >= 50) {
+      for (int positionCounter = 0; positionCounter < 40; positionCounter ++) {
+        lcd.setCursor(1, 0);
+        lcd.scrollDisplayLeft();  //Essa é a função que faz as letras se deslocarem
+        lcd.print("Insira a Sonda ou verifique a Crimpagem");
+        delay(250);
       }
-    else {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("sonda OK");
-    lcd.setCursor(10,0);
-    lcd.print(celsius);
-    lcd.setCursor(14,0);
-    lcd.print("C");
-    lcd.setCursor(3,1);
-    lcd.print("SN:");
-    lcd.setCursor(7,1);
-    for( i = 4; i > 0; i--) {
-    lcd.print(addr[i], HEX);
     }
+    else {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("sonda OK");
+      lcd.setCursor(10, 0);
+      lcd.print(celsius);
+      lcd.setCursor(14, 0);
+      lcd.print("C");
+      lcd.setCursor(3, 1);
+      lcd.print("SN:");
+      lcd.setCursor(7, 1);
+      for ( i = 4; i > 0; i--) {
+        lcd.print(addr[i], HEX);
+      }
     }
     delay(1000);
     return;
-  } 
-  
-/*
-    lcd.print(addr[3], HEX);
-    lcd.setCursor(7,1);
-    lcd.print(addr[2], HEX);
-    lcd.setCursor(8,1);
-    lcd.print(addr[1], HEX);
-    */
+  }
 
-
-  // the first ROM byte indicates which chip
+  // O primeiro byte da ROM indica o modelo do chip, apenas para título de curiosidade.
   switch (addr[0]) {
     case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
+      Serial.println("  Chip = DS18S20");
       type_s = 1;
       break;
     case 0x28:
@@ -126,34 +101,7 @@ void loop(void) {
       type_s = 0;
       break;
     default:
-      Serial.println("Device is not a DS18x20 family device.");
+      Serial.println("O Dispositivo não pertence à familia DS18x20.");
       return;
   }
-
-
-  
-  }
-/*
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-  celsius = (float)raw / 16.0;
-    Serial.print("  Temperatura = ");
-  Serial.print(celsius);
-  }*/
+}
